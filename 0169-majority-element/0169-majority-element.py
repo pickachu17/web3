bfdb0b7819e6318720a40b315cf236c47b9bcd7b
pickachu17 <86123996+pickachu17@users.noreply.github.com>
2023-08-23 21:14:29 +0530

    Time: 164 ms (59.25%), Space: 18 MB (26.42%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,11 +1,3 @@
-
-
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        sol = None
-        cnt = 0
-        for i in nums:
-            if cnt == 0:
-                sol = i
-            cnt += (1 if i == sol else -1)
-        return sol
+        return Counter(nums).most_common()[0][0]
